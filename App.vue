75763ee2272bb9f43786c4dc6092d05b7678ca8b
AndersonCarvajal <andersoncarvajalromero30@gmail.com>
2026-07-13 10:20:49 -0400

    Add files via upload

---
+++ App.vue
@@ -0,0 +1,19 @@
+<template>
+  <main>
+    <h1>Mi contenido reutilizable</h1>
+
+    <Card
+      titulo="Producto 1"
+      descripcion="Esta es una tarjeta reutilizable en Vue."
+    />
+
+    <Card
+      titulo="Producto 2"
+      descripcion="Puedo cambiar los datos sin crear otro componente."
+    />
+  </main>
+</template>
+
+<script setup>
+import Card from './components/Card.vue'
+</script>
