50fbb3974ddb66ba6321b4a36bc157a758dd3ec6
AndersonCarvajal <andersoncarvajalromero30@gmail.com>
2026-07-15 09:06:28 -0400

    Update Ander EncabezadoApp.vue

--- src/components/EncabezadoApp.vue
+++ src/components/EncabezadoApp.vue
@@ -1,1 +1,37 @@
+<script setup>
+defineProps({
+  titulo: {
+    type: String,
+    required: true
+  },
+  subtitulo: {
+    type: String,
+    required: true
+  }
+})
+</script>
+
+<template>
+  <header class="encabezado">
+    <h1>{{ titulo }}</h1>
+    <p>{{ subtitulo }}</p>
+  </header>
+</template>
+
+<style scoped>
+.encabezado {
+  padding: 24px;
+  background-color: #1a1717;
+  color: white;
+  text-align: center;
+}
+
+.encabezado h1 {
+  margin: 0;
+}
+
+.encabezado p {
+  margin: 8px 0 0;
+}
+</style>
 
